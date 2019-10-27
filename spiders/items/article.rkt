633b3756cfa028a16cac6814d1337bbbe0690af0
jooojoe <wuweiru@apluslabs.com>
2019-10-27 13:04:49 +0800

    response struct

--- spiders/items/article.rkt
+++ spiders/items/article.rkt
@@ -1,1 +1,18 @@
 #lang racket
+(require
+    rapider
+    txexpr)
+
+(provide
+  article-item)
+
+(define article-item
+  (item
+    (item-field #:name "title" #:xpath "/html/body/div[2]/div[2]/div/div[1]/h1/text()")
+    (item-field #:name "date" #:xpath "/html/body/div[2]/div[2]/div/div[1]/div/div/div/span[1]/text()")
+    (item-field #:name "author" #:xpath "/html/body/div[2]/div[2]/div/div[1]/div/div/div/span[2]/a/text()")
+    (item-field #:name "tip" #:xpath "/html/body/div[2]/div[2]/div/div[1]/div/div/div/span[3]/text()")
+    (item-field
+      #:name "content"
+      #:xpath "/html/body/div[2]/div[2]/div/div[2]/div[2]"
+      #:filter (λ (x) (xexpr->string (car x))))))
--- spiders/spider.rkt
+++ spiders/spider.rkt
@@ -2,7 +2,9 @@
 
 (require
   json
-  rapider)
+  rapider
+  html-parsing
+  "items/article.rkt")
 
 ; article-new-default-font
 
@@ -35,10 +37,11 @@
       (name "sensor")
       (header '("User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) 
               AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"))
+      (base-url "https://www.eefocus.com")
       (base-api-url "https://www.eefocus.com/article/index/more.articles?p=1&channel=")
       (start-urls (map 
         (lambda (x) 
-          (string-append base-api-url (number->string x))) (range 1 15))))
+          (string-append base-api-url (number->string x))) (range 1 14))))
 
     (define/public (start)
       (for ([url start-urls])
@@ -56,17 +59,19 @@
       (close-output-port out))
 
     (define/public (parse-channel rsp)
-      (define url (response-url rsp))
+      (define origin-url (response-url rsp))
       (define content (response-content rsp))
-      (define ret-hash 
+      (define ret-hash
         (with-input-from-string content (λ () (read-json))))
       (for ([data (hash-ref ret-hash 'data)])
-        (request this (string-append "https://www.eefocus.com" (hash-ref data 'url)) 'parse-page))
-      (display url)
+        (let ([url (string-append base-url (hash-ref data 'url))])
+         (request this url 'parse-page)))
       (void))
 
     (define/public (parse-page rsp)
-      (void))
+      (define origin-url (response-url rsp))
+      (define content (response-content rsp))
+      (displayln (article-item (html->xexp content))))
 
 
   (super-new)))
